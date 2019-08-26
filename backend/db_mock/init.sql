46c3a68df9c286ee3f095693accf5e5955117690
Joakim Hulthe <joakim@hulthe.net>
2019-08-26 20:13:45 +0200

    Polish database mock

--- backend/db_mock/init.sql
+++ backend/db_mock/init.sql
@@ -33,49 +33,100 @@ COPY public.events (id, title, background, location, start_time, end_time, price
 23	My Event 23	http://imgur.ru	Hubben 2.1	2019-12-02 17:01:00	2019-12-02 23:59:59	0	t
 24	My Event 24	http://imgur.ru	Hubben 2.1	2019-12-16 17:01:00	2019-12-16 23:59:59	0	f
 \.
-
--- Data for Name: inventory; Type: TABLE DATA; Schema: public; Owner: postgres
-COPY public.inventory (name, price) FROM stdin;
-Dr. Pepper	\N
-\.
-
--- Data for Name: transactions; Type: TABLE DATA; Schema: public; Owner: postgres
-COPY public.transactions (id, amount, description, "time") FROM stdin;
-1	-120	AxFood-inköp	2019-04-18 16:35:42.56635
-\.
-
--- Data for Name: transaction_items; Type: TABLE DATA; Schema: public; Owner: postgres
-COPY public.transaction_items (id, transaction_id, item_name, item_price, change) FROM stdin;
-1	1	Dr. Pepper	6	added
-2	1	Dr. Pepper	6	added
-3	1	Dr. Pepper	6	added
-4	1	Dr. Pepper	6	added
-5	1	Dr. Pepper	6	added
-6	1	Dr. Pepper	6	added
-7	1	Dr. Pepper	6	added
-8	1	Dr. Pepper	6	added
-9	1	Dr. Pepper	6	added
-10	1	Dr. Pepper	6	added
-11	1	Dr. Pepper	6	added
-12	1	Dr. Pepper	6	added
-13	1	Dr. Pepper	6	added
-14	1	Dr. Pepper	6	added
-15	1	Dr. Pepper	6	added
-16	1	Dr. Pepper	6	added
-17	1	Dr. Pepper	6	added
-18	1	Dr. Pepper	6	added
-19	1	Dr. Pepper	6	added
-20	1	Dr. Pepper	6	added
-21	1	Dr. Pepper	6	added
-22	1	Dr. Pepper	6	added
-23	1	Dr. Pepper	6	added
-24	1	Dr. Pepper	6	added
-\.
+-- ##################
 
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 COPY public.users (name, display_name, salted_pass, hash_iterations) FROM stdin;
 laggit	LaggIT	8790b5087a6186e4bd9c8a664be012105881bbe124d5499700aad7bb2947b7563ba88bc932bbde2e7f971b9ad5fccebb17d4ace7716c420faf0ed4af3d424735e3f5c9d1d0e988666b74d7b378872460bf721cb5ef307de77e3d358479a04a6306bb88ef5569eac4c2dc86c82b19378f3cb617a4de1a2bda5894a4f763bad432	10000
 \.
+-- ##################
+
+-- Add inventory index
+COPY public.inventory (id, price, name) FROM stdin;
+01	\N	Algrens Bilar, Orginal
+02	\N	Banana Skids
+03	6	Chokladboll, Daim
+04	6	Chokladboll, Kokos
+05	6	Chokladboll, Pärlsocker
+06	12	Chokladrullar
+07	12	Chokladrullar, Daim
+08	12	Chokladrullar, Mint
+09	6	Djungelvrål
+10	12	Gott & Blandat
+11	6	Hallonlakritsskalle
+12	6	Haribo Nallar
+13	6	Haribo Persikor
+14	6	Jättesalt
+15	6	Kexchoklad
+16	12	Kinasnacks
+17	6	Lakrisal
+18	6	Lollipop Fruit
+19	6	Mars
+20	6	Nappar, Fruit
+21	6	Nappar, Kola
+22	6	Nappar, Lakrits
+23	6	Nappar, Zour
+24	6	Pingvinstång, Jordgubb
+25	6	Pingvinstång, Mint
+26	6	Tutti Frutti
+27	6	Wasa Sandwich
+28	12	Tyrkisk Peber
+29	6	ZOO Apor
+30	6	Coca-Cola
+31	6	Coca-Cola Vanilla
+32	6	Coca-Cola Zero
+33	6	Dr. Pepper
+34	6	Fanta
+35	6	Hallonsoda
+36	6	Loka, Citron
+37	6	Loka, Päron
+38	6	Mountaindew
+39	6	Pärondryck
+40	6	Ramlösa, Granatäpple
+41	6	Ramlösa, Hallon & björnbär
+42	6	Smakis, Päron
+43	6	Smakis, Äpple
+44	6	Sockerdricka
+45	6	Sprite, Citron
+46	6	Trocadero
+47	6	Zingo
+48	6	Zingo, Tropical
+49	6	Vimto
+\.
+-- ##################
+
+-- Add some transactions
+COPY public.transactions (id, amount, description) FROM stdin;
+1	-500	AxFood-inköp
+2	18	Försäljning
+3	24	Försäljning
+\.
+
+COPY public.transaction_bundles (transaction_id, id, change) FROM stdin;
+1	1	24
+1	2	30
+1	3	24
+1	4	12
+1	5	50
+1	6	8
+2	7	-2
+2	8	-1
+3	9	-5
+\.
+
+COPY public.transaction_items (bundle_id, item_id) FROM stdin;
+1	30
+2	33
+3	34
+4	49
+5	25
+6	17
+7	25
+7	25
+8	34
+9	49
+\.
 
 REFRESH MATERIALIZED VIEW public.inventory_stock;
+-- ##################
 
--- backend/db_mock/populate.sh
+++ backend/db_mock/populate.sh
@@ -1,1 +1,8 @@
+export DOCKER_NAME="$1"
+
+if [ -z "$DOCKER_NAME" ]; then
+	echo "Please specify the name/id of the database container."
+	exit 1
+fi
+
 docker exec -u postgres -i "$1" psql -d laggit < init.sql
