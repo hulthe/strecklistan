871f2d4b02e8f1ee0b8d0b462b1abdc4bbdb83d7
Joakim Hulthe <joakim@hulthe.net>
2019-11-12 13:24:38 +0100

    Serve tailwind locally

--- frontend/src/generated/css_classes.rs
+++ frontend/src/generated/css_classes.rs
@@ -1,5 +1,11 @@
 use css_typegen::css_typegen;
 
+// NOTE: Remember to edit index.html when adding new css-files!
+
 // Generate rust types for css-classes.
 // Used for autocompletion and extra compile-time checks.
-css_typegen!("frontend/static/styles.css", "frontend/static/tailwind.css");
+css_typegen!(
+    "frontend/static/styles.css",
+    "frontend/static/tailwind.css",
+    "frontend/static/heart_spinner.css",
+);
