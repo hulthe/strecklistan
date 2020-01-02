2ea53e046e9fc6c4d921a603a317664991fce496
Joakim Hulthe <joakim@hulthe.net>
2020-01-02 17:59:56 +0100

    Improve performance of transaction history page
    - pre-render transaction nodes
    - add a view limit (default 50)

--- frontend/src/page/transactions.rs
+++ frontend/src/page/transactions.rs
@@ -5,7 +5,10 @@ use laggit_api::inventory::InventoryItemStock as InventoryItem;
 use laggit_api::transaction::{Transaction, TransactionId};
 use seed::prelude::*;
 use seed::{fetch::FetchObject, *};
+use std::collections::HashMap;
 use std::ops::Deref;
+
+const VIEW_COUNT_CHUNK: usize = 50;
 
 #[derive(Clone, Debug)]
 pub enum TransactionsMsg {
@@ -14,28 +17,70 @@ pub enum TransactionsMsg {
     SetShowDelete(bool),
     SetShowLeftPanel(bool),
     FilterMenuMsg(FilterMenuMsg),
+    IncreaseViewLimit,
 }
 
 #[derive(Clone)]
 pub struct TransactionsPage {
     show_delete: bool,
     show_left_panel: bool,
+    view_limit: usize,
     filter_menu: FilterMenu,
+
+    // Indexes into global.transaction_history
+    filtered_transactions: Vec<usize>,
+
+    // Left is rendered without delete button, right is rendered with
+    rendered_transactions: HashMap<TransactionId, (Node<TransactionsMsg>, Node<TransactionsMsg>)>,
 }
 
 impl TransactionsPage {
-    pub fn new(_global: &StateReady) -> Self {
-        TransactionsPage {
+    pub fn new(global: &StateReady) -> Self {
+        let mut page = TransactionsPage {
             show_delete: false,
             show_left_panel: false,
+            view_limit: VIEW_COUNT_CHUNK,
             filter_menu: FilterMenu::new(vec!["datum", "klockslag", "summa", "debet", "kredit"]),
-        }
+            filtered_transactions: vec![],
+            rendered_transactions: HashMap::new(),
+        };
+        page.filter_transactions(global);
+        page
+    }
+
+    /// Rebuild self.filtered_transactions and make sure all transactions it references are
+    /// pre-rendered in self.rendered_transactions
+    fn filter_transactions(&mut self, global: &StateReady) {
+        self.filtered_transactions = global
+            .transaction_history
+            .iter()
+            .enumerate()
+            .filter_map(|(i, tr)| {
+                if self.filter_menu.filter(&[
+                    &tr.time.format("%Y-%m-%d"),                                   // datum
+                    &tr.time.format("%H:%M:%S"),                                   // klockslag
+                    &tr.amount,                                                    // summa
+                    &global.book_accounts.get(&tr.debited_account).unwrap().name,  // debet
+                    &global.book_accounts.get(&tr.credited_account).unwrap().name, // kredit
+                ]) {
+                    self.rendered_transactions.entry(tr.id).or_insert_with(|| {
+                        (
+                            view_transaction(global, tr, false),
+                            view_transaction(global, tr, true),
+                        )
+                    });
+                    Some(i)
+                } else {
+                    None
+                }
+            })
+            .collect();
     }
 
     pub fn update(
         &mut self,
         msg: TransactionsMsg,
-        _global: &mut StateReady,
+        global: &mut StateReady,
         orders: &mut impl Orders<Msg>,
     ) {
         let mut orders_local = orders.proxy(|msg| Msg::TransactionsMsg(msg));
@@ -64,105 +109,37 @@ impl TransactionsPage {
             TransactionsMsg::SetShowLeftPanel(show_left_panel) => {
                 self.show_left_panel = show_left_panel;
             }
-            TransactionsMsg::FilterMenuMsg(msg) => self.filter_menu.update(
-                msg,
-                &mut orders_local.proxy(|msg| TransactionsMsg::FilterMenuMsg(msg)),
-            ),
+            TransactionsMsg::FilterMenuMsg(msg) => {
+                self.filter_menu.update(
+                    msg,
+                    &mut orders_local.proxy(|msg| TransactionsMsg::FilterMenuMsg(msg)),
+                );
+                self.view_limit = VIEW_COUNT_CHUNK; // reset view limit
+                self.filter_transactions(global);
+            }
+            TransactionsMsg::IncreaseViewLimit => {
+                self.view_limit += VIEW_COUNT_CHUNK;
+                self.filter_transactions(global);
+            }
         }
     }
 
     pub fn view(&self, global: &StateReady) -> Node<Msg> {
-        let view_transaction = |transaction: &Transaction| {
-            div![
-                class![C.transaction_view],
-                p![
-                    span![format!("#{} ", transaction.id)],
-                    span![transaction
-                        .description
-                        .as_ref()
-                        .map(|s| s.as_str())
-                        .unwrap_or("Transaktion")],
-                    if self.show_delete {
-                        button![
-                            class![C.transaction_view_delete_button],
-                            simple_ev(
-                                Ev::Click,
-                                TransactionsMsg::DeleteTransaction(transaction.id)
-                            ),
-                            "✖",
-                        ]
-                    } else {
-                        empty![]
-                    }
-                ],
-                p![format!("{}", transaction.time.format("%Y-%m-%d %H:%M:%S"))],
-                p![
-                    class![C.mt_2],
-                    span!["Debet: "],
-                    span![
-                        class![C.font_bold],
-                        global
-                            .book_accounts
-                            .get(&transaction.debited_account)
-                            .map(|acc| acc.name.as_str())
-                            .unwrap_or("[MISSING]")
-                    ],
-                ],
-                p![
-                    class![C.mt_2, C.mb_2],
-                    span!["Kredit: "],
-                    span![
-                        class![C.font_bold],
-                        global
-                            .book_accounts
-                            .get(&transaction.credited_account)
-                            .map(|acc| acc.name.as_str())
-                            .unwrap_or("[MISSING]")
-                    ],
-                ],
-                transaction
-                    .bundles
-                    .iter()
-                    .map(|bundle| {
-                        let mut items = bundle.item_ids.keys().map(|id| &global.inventory[id]);
-
-                        // TODO: Properly display more complicated bundles
-
-                        let (item_name, item_price) = match items.next().map(|rc| rc.deref()) {
-                            None => (None, 0),
-                            Some(InventoryItem { name, price, .. }) => {
-                                (Some(name.as_str()), price.unwrap_or(0))
-                            }
-                        };
-
-                        let name = bundle
-                            .description
-                            .as_ref()
-                            .map(|s| s.as_str())
-                            .unwrap_or(item_name.unwrap_or("[NAMN SAKNAS]"));
-                        let price = bundle.price.unwrap_or(item_price.into());
-                        p![
-                            class![C.transaction_entry],
-                            span![
-                                class![C.transaction_entry_item_name],
-                                format!("{}x {}", -bundle.change, name),
-                            ],
-                            span![
-                                class![C.transaction_entry_item_price],
-                                format!("{}:-", price),
-                            ],
-                        ]
-                    })
-                    .collect::<Vec<_>>(),
-                p![
-                    span!["Totalt: "],
-                    span![
-                        class![C.transaction_entry_item_price],
-                        format!("{}:-", transaction.amount),
-                    ],
-                ],
-            ]
-        };
+        let transaction_list: Vec<_> = self
+            .filtered_transactions
+            .iter()
+            .take(self.view_limit)
+            .map(|&i| &global.transaction_history[i])
+            .map(|tr| &self.rendered_transactions[&tr.id])
+            .map(|(without_button, with_button)| {
+                if self.show_delete {
+                    with_button
+                } else {
+                    without_button
+                }
+                .clone()
+            })
+            .collect();
 
         div![
             class![C.transactions_page],
@@ -197,24 +174,118 @@ impl TransactionsPage {
             div![
                 class![C.transactions_list],
                 button![
-                    class!["transaction_page_show_delete"],
+                    class![C.transaction_page_show_delete],
                     "Radera transaktioner?",
                     simple_ev(Ev::Click, TransactionsMsg::SetShowDelete(!self.show_delete)),
                 ],
-                global
-                    .transaction_history
-                    .iter()
-                    .filter(|tr| self.filter_menu.filter(&[
-                        &tr.time.format("%Y-%m-%d"),                                   // datum
-                        &tr.time.format("%H:%M:%S"),                                   // klockslag
-                        &tr.amount,                                                    // summa
-                        &global.book_accounts.get(&tr.debited_account).unwrap().name,  // debet
-                        &global.book_accounts.get(&tr.credited_account).unwrap().name, // kredit
-                    ]))
-                    .map(|t| view_transaction(t))
-                    .collect::<Vec<_>>(),
-            ]
+                transaction_list,
+                if self.view_limit < self.filtered_transactions.len() {
+                    button![
+                        class![C.wide_button],
+                        "Visa fler",
+                        simple_ev(Ev::Click, TransactionsMsg::IncreaseViewLimit),
+                    ]
+                } else {
+                    empty![]
+                },
+            ],
         ]
         .map_message(|msg| Msg::TransactionsMsg(msg))
     }
+}
+
+fn view_transaction(
+    global: &StateReady,
+    transaction: &Transaction,
+    show_delete: bool,
+) -> Node<TransactionsMsg> {
+    div![
+        class![C.transaction_view],
+        p![
+            span![format!("#{} ", transaction.id)],
+            span![transaction
+                .description
+                .as_ref()
+                .map(|s| s.as_str())
+                .unwrap_or("Transaktion")],
+            if show_delete {
+                button![
+                    class![C.transaction_view_delete_button],
+                    simple_ev(
+                        Ev::Click,
+                        TransactionsMsg::DeleteTransaction(transaction.id)
+                    ),
+                    "✖",
+                ]
+            } else {
+                empty![]
+            }
+        ],
+        p![format!("{}", transaction.time.format("%Y-%m-%d %H:%M:%S"))],
+        p![
+            class![C.mt_2],
+            span!["Debet: "],
+            span![
+                class![C.font_bold],
+                global
+                    .book_accounts
+                    .get(&transaction.debited_account)
+                    .map(|acc| acc.name.as_str())
+                    .unwrap_or("[MISSING]")
+            ],
+        ],
+        p![
+            class![C.mt_2, C.mb_2],
+            span!["Kredit: "],
+            span![
+                class![C.font_bold],
+                global
+                    .book_accounts
+                    .get(&transaction.credited_account)
+                    .map(|acc| acc.name.as_str())
+                    .unwrap_or("[MISSING]")
+            ],
+        ],
+        transaction
+            .bundles
+            .iter()
+            .map(|bundle| {
+                let mut items = bundle.item_ids.keys().map(|id| &global.inventory[id]);
+
+                // TODO: Properly display more complicated bundles
+
+                let (item_name, item_price) = match items.next().map(|rc| rc.deref()) {
+                    None => (None, 0),
+                    Some(InventoryItem { name, price, .. }) => {
+                        (Some(name.as_str()), price.unwrap_or(0))
+                    }
+                };
+
+                let name = bundle
+                    .description
+                    .as_ref()
+                    .map(|s| s.as_str())
+                    .unwrap_or(item_name.unwrap_or("[NAMN SAKNAS]"));
+                let price = bundle.price.unwrap_or(item_price.into());
+                p![
+                    class![C.transaction_entry],
+                    span![
+                        class![C.transaction_entry_item_name],
+                        format!("{}x {}", -bundle.change, name),
+                    ],
+                    span![
+                        class![C.transaction_entry_item_price],
+                        format!("{}:-", price),
+                    ],
+                ]
+            })
+            .collect::<Vec<_>>(),
+        p![
+            span!["Totalt: "],
+            span![
+                class![C.transaction_entry_item_price],
+                format!("{}:-", transaction.amount),
+            ],
+        ],
+    ]
 }
