e3b570ea395268c0ca9e20dd901f304dcea824f8
joakia <joakia@student.chalmers.se>
2021-06-03 22:04:29 +0200

    fix formating

--- frontend/src/components/checkout.rs
+++ frontend/src/components/checkout.rs
@@ -340,18 +340,15 @@ impl Checkout {
                 if self.transaction_bundles.is_empty() {
                     button![
                         C![C.greyed_out, C.wide_button, C.border_on_focus],
-                        div![
-                            style! {
-                                St::Position => "absolute",
-                                St::MarginTop => "-0.25em",
-                                St::Filter => "invert(100%)",
-                            },
-                        ],
+                        div![style! {
+                            St::Position => "absolute",
+                            St::MarginTop => "-0.25em",
+                            St::Filter => "invert(100%)",
+                        },],
                         attrs! { At::Disabled => true },
                         "Slutför Köp",
-                    ] 
-                }
-                else {
+                    ]
+                } else {
                     button![
                         C![C.wide_button, C.border_on_focus],
                         simple_ev(Ev::Click, CheckoutMsg::ConfirmPurchase),
